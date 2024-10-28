6d172ad2ad1a0e4b2fb783253ff8a33e2be952fe
uiuiuiui15 <dr.venitus@gmail.com>
2024-10-28 22:19:26 +0500

    Add project files.

---
+++ Tyuiu.VorobevAD.Sprint2.Task0.V26.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.VorobevAD.Sprint2.Task0.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.VorobevAD.Sprint2.Task0.V26/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.VorobevAD.Sprint2.Task0.V26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

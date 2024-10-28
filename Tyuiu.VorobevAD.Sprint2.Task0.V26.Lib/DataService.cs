0436568e594d6cf6894c423af4f175022e3d4089
uiuiuiui15 <dr.venitus@gmail.com>
2024-10-28 23:02:55 +0500

    test

--- Tyuiu.VorobevAD.Sprint2.Task0.V26.Lib/DataService.cs
+++ Tyuiu.VorobevAD.Sprint2.Task0.V26.Lib/DataService.cs
@@ -1,13 +1,10 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint2;
+﻿
 
 
 namespace Tyuiu.VorobevAD.Sprint2.Task0.V26.Lib
 {
-    public class DataService : tyuiu.cources.programming.interfaces.Sprint2.ISprint2Task0V26
+    public class DataService
     {
-        public bool[] GetCompareOperations(int x, int y)
-        {
-            throw new NotImplementedException();
-        }
+        
     }
 }

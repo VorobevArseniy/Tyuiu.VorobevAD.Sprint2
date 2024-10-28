c3a93d063f53bcde598208d521381f8517d0d46c
uiuiuiui15 <dr.venitus@gmail.com>
2024-10-28 22:30:48 +0500

    c

--- Tyuiu.VorobevAD.Sprint2.Task0.V26.Lib/DataService.cs
+++ Tyuiu.VorobevAD.Sprint2.Task0.V26.Lib/DataService.cs
@@ -5,18 +5,8 @@ namespace Tyuiu.VorobevAD.Sprint2.Task0.V26.Lib
 {
     public class DataService : tyuiu.cources.programming.interfaces.Sprint2.ISprint2Task0V26
     {
-        bool[] ISprint2Task0V26.GetCompareOperations(int x, int y)
+        public bool[] GetCompareOperations(int x, int y)
         {
-
-        return new bool[]
-        {
-            x == y,              // 1. False
-            !(x != y),           // 2. False
-            !(x < y),            // 3. False
-            x > y,               // 4. True
-            !(x <= y),           // 5. True
-            x >= y               // 6. True
-        };
             throw new NotImplementedException();
         }
     }

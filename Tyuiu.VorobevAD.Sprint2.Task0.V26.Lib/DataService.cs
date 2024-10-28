bf89e389251cd547652012a188dc1bd8ed85b046
uiuiuiui15 <dr.venitus@gmail.com>
2024-10-28 23:06:30 +0500

    aasdf

--- Tyuiu.VorobevAD.Sprint2.Task0.V26.Lib/DataService.cs
+++ Tyuiu.VorobevAD.Sprint2.Task0.V26.Lib/DataService.cs
@@ -7,6 +7,15 @@ namespace Tyuiu.VorobevAD.Sprint2.Task0.V26.Lib
     {
         public bool[] GetCompareOperations(int x, int y)
         {
+        return new bool[]
+            {
+            x == y,              // 1. False
+            !(x != y),           // 2. False
+            !(x < y),            // 3. False
+            x > y,               // 4. True
+            !(x <= y),           // 5. True
+            x >= y               // 6. True
+            };
             throw new NotImplementedException();
         }
     }

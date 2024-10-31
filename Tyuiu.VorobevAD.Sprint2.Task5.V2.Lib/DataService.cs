4191a7d6a5751f7c6ef88a5502762c1f408aceeb
uiuiuiui15 <dr.venitus@gmail.com>
2024-10-31 12:56:16 +0500

    a

---
+++ Tyuiu.VorobevAD.Sprint2.Task5.V2.Lib/DataService.cs
@@ -0,0 +1,42 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.VorobevAD.Sprint2.Task5.V2.Lib
+{
+    public class DataService : tyuiu.cources.programming.interfaces.Sprint2.ISprint2Task5V2
+    {
+        public string FindMonthSeason(int value)
+        {
+            string season = "";
+
+            switch (value)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    season = "Зима";
+                    break;
+
+                case 3:
+                case 4:
+                case 5:
+                    season = "Весна";
+                    break;
+
+                case 6:
+                case 7:
+                case 8:
+                    season = "Лето";
+                    break;
+
+                case 9:
+                case 10:
+                case 11:
+                    season = "Осень";
+                    break;
+            }
+
+            return season;
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tyuiu.VorobevAD.Sprint2.Task5.V2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.VorobevAD.Sprint2.Task5.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
